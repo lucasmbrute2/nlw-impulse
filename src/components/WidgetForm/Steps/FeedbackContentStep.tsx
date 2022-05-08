c7067de404a82b3223d9dbe79943da4392718f2e
Lucas <lucasmbrute614@gmail.com>
2022-05-08 17:46:32 -0300

    feat: add send feedback button

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,38 +1,53 @@
 import { ArrowLeft } from "phosphor-react";
-import { FeedbackType, feedbackTypes, } from "..";
+import { FeedbackType, feedbackTypes } from "..";
 import { CloseButton } from "../../CloseButton";
 
 interface FeedbackContentStepProps {
-  feedbackType: FeedbackType;
-  onFeedbackRestartRequested: () => void;
+	feedbackType: FeedbackType;
+	onFeedbackRestartRequested: () => void;
 }
 
-export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested }: FeedbackContentStepProps) {
-  const { image, title } = feedbackTypes[feedbackType];
+export function FeedbackContentStep({
+	feedbackType,
+	onFeedbackRestartRequested,
+}: FeedbackContentStepProps) {
+	const { image, title } = feedbackTypes[feedbackType];
 
-  return (
-    <>
-      <header>
-        <button
-          type="button"
-          className="top-5 left-5 absolute text-zinc-400 hover:text-zinc-100"
-          onClick={onFeedbackRestartRequested}
-        >
-          <ArrowLeft weight="bold" className="w-4 h-4" />
-        </button>
-        <span className="text-xl leading-6 flex items-center gap-2">
-          <img src={image.source} alt={image.alt} className="w-6 h-6" />
-          {title}
-        </span>
-        <CloseButton />
-      </header>
+	return (
+		<>
+			<header>
+				<button
+					type="button"
+					className="top-5 left-5 absolute text-zinc-400 hover:text-zinc-100"
+					onClick={onFeedbackRestartRequested}
+				>
+					<ArrowLeft weight="bold" className="w-4 h-4" />
+				</button>
+				<span className="text-xl leading-6 flex items-center gap-2">
+					<img
+						src={image.source}
+						alt={image.alt}
+						className="w-6 h-6"
+					/>
+					{title}
+				</span>
+				<CloseButton />
+			</header>
 
-      <form className="my-4 w-full">
-        <textarea
-          className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
-          placeholder="Conte com detalhes o que está acontecendo"
-        />
-      </form>
-    </>
-  );
+			<form className="my-4 w-full">
+				<textarea
+					className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
+					placeholder="Conte com detalhes o que está acontecendo"
+				/>
+				<footer className="flex gap-2 mt-2">
+					<button
+						type="submit"
+						className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"
+					>
+						Enviar feedback
+					</button>
+				</footer>
+			</form>
+		</>
+	);
 }
